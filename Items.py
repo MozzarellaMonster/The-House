5754af09b1a358280c24177441eb37ef13084f08
MozzMon <mozzarellamonster@users.noreply.github.com>
2021-06-25 17:25:48 -0600

    Made items in Items into dictionaries

--- Items.py
+++ Items.py
@@ -1,12 +1,36 @@
 class items:
-    def silver_fork():
-        pass
+    #def silver_fork():
+    #    pass
 
-    def old_gear():
-        pass
+    #def old_gear():
+    #    pass
 
-    def attic_key():
-        pass
+    #def attic_key():
+    #    pass
 
-    def flashlight():
-        pass
+    #def flashlight():
+    #    pass
+
+    silver_fork = {
+        "Item name": "Silver Fork",
+        "Found in": "Kitchen",
+        "Used in": "PASS"
+    }
+
+    old_gear = {
+        "Item name": "Old Gear",
+        "Found in": "Attic",
+        "Used in": "Study"
+    }
+
+    attic_key = {
+        "Item name": "Attic Key",
+        "Found in": "Foyer",
+        "Used in": "Attic"
+    }
+
+    flashlight = {
+        "Item name": "Flashlight",
+        "Found in": "Bedroom",
+        "Used in": "Basement"
+    }
