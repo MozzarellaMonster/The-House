476e1cf0f21af5b357dcd2307c17c085d3a3c393
MozzMon <mozzarellamonster@users.noreply.github.com>
2021-06-27 23:55:27 -0600

    Edit the intro text to better reflect the theme and objective of the game.

--- Text-based Adventure Game.py
+++ Text-based Adventure Game.py
@@ -2,17 +2,15 @@ from Options import options
 from Missing import missing
 
 class main:
-    print("Hello! Welcome to The Haunted House!")
-    print("Your choices will matter in this deadly maze.")
-    print("Will you solve the mystery of this old house and make it out alive?")
-    print("Or will you be forever lost wandering this cursed labyrinth?\n")
+    print("Hello! Welcome to The House!")
+    print("A mystery awaits you in this accursed abode.")
+    print("Will you make it out alive or will The House claim another life?")
 
     print("You are a journalist investigating the house at the end of the street on Willow Road.")
-    print("Throughout your time staying in this town, the house has been the focal point of many stories.")
-    print("Stories that often involved actual missing person cases. You're here to see if any of those stories")
-    print("can actually be proven.\n\n")
+    print("Throughout your time staying in this town, this house has been the focal point of many stories.")
+    print("Stories that give the impression there is more going on than meets the eye.")
 
-    print("You find yourself standing on the porch of the old house. You wonder how this house hasn't been demolished")
-    print("yet. You grip the ice-cold doorknob despite it being a warm, sunny summer afternoon. You pull your hand back, surprised.")
+    print("You find yourself standing on the porch of the old house. You wonder how this house hasn't been demolished yet.")
+    print("You grip the ice-cold doorknob despite it being a warm, sunny summer afternoon. You pull your hand back, surprised.")
 
     options.porch()
