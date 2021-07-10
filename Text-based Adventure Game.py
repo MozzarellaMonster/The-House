727d2d1b771d64a60341933a7a08cf83b7c73cca
MozzMon <mozzarellamonster@users.noreply.github.com>
2021-07-10 00:57:37 -0600

    Added some newlines for better readability

--- Text-based Adventure Game.py
+++ Text-based Adventure Game.py
@@ -12,15 +12,15 @@ class main:
     print("   )_(   |/     \|(_______/  |/     \|(_______)(_______)\_______)(_______/")
     print("                                                                          ")
     
-    print("Hello! Welcome to The House!")
+    print("\nHello! Welcome to The House!")
     print("A mystery awaits you in this accursed abode.")
     print("Will you make it out alive or will The House claim another life?")
 
-    print("You are a journalist investigating the house at the end of the street on Willow Road.")
+    print("\nYou are a journalist investigating the house at the end of the street on Willow Road.")
     print("Throughout your time staying in this town, this house has been the focal point of many stories.")
     print("Stories that give the impression there is more going on than meets the eye.")
 
-    print("You find yourself standing on the porch of the old house. You wonder how this house hasn't been demolished yet.")
+    print("\nYou find yourself standing on the porch of the old house. You wonder how this house hasn't been demolished yet.")
     print("You grip the ice-cold doorknob despite it being a warm, sunny summer afternoon. You pull your hand back, surprised.")
 
     options.porch()
