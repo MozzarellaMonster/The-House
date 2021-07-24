4ec0e9d0002a3febfd9d1dbd732eaed33b0309e4
MozzMon <mozzarellamonster@users.noreply.github.com>
2021-07-24 04:05:16 -0600

    Removed several lines of code and made it into a function within Options for easier execution.

--- Text-based Adventure Game.py
+++ Text-based Adventure Game.py
@@ -1,25 +1,5 @@
 from Options import options
 
 class main:
-    print("_________          _______             _______           _______  _______ ")
-    print("\__   __/|\     /|(  ____ \  |\     /|(  ___  )|\     /|(  ____ \(  ____ \"")
-    print("   ) (   | )   ( || (    \/  | )   ( || (   ) || )   ( || (    \/| (    \/")
-    print("   | |   | (___) || (__      | (___) || |   | || |   | || (_____ | (__    ")
-    print("   | |   |  ___  ||  __)     |  ___  || |   | || |   | |(_____  )|  __)   ")
-    print("   | |   | (   ) || (        | (   ) || |   | || |   | |      ) || (      ")
-    print("   | |   | )   ( || (____/\  | )   ( || (___) || (___) |/\____) || (____/\"")
-    print("   )_(   |/     \|(_______/  |/     \|(_______)(_______)\_______)(_______/")
-    print("                                                                          ")
     
-    print("\nHello! Welcome to The House!")
-    print("A mystery awaits you in this accursed abode.")
-    print("Will you make it out alive or will The House claim another life?")
-
-    print("\nYou are a journalist investigating the house at the end of the street on Willow Road.")
-    print("Throughout your time staying in this town, this house has been the focal point of many stories.")
-    print("Stories that give the impression there is more going on than meets the eye.")
-
-    print("\nYou find yourself standing on the porch of the old house. You wonder how this house hasn't been demolished yet.")
-    print("You grip the ice-cold doorknob despite it being a warm, sunny summer afternoon. You pull your hand back, surprised.")
-
-    options.porch()
+    options.start()
